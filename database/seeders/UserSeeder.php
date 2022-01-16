13566b33237b727e4ec218f78d686f9a5c31f9cc
ibrahimcadirci <ibrahimh.cadirci@gmail.com>
2022-01-16 15:14:44 +0300

    Creates UserSeeder and UserFactory

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       
+       
+        User::factory()
+        ->count(5)
+        ->state(new Sequence(
+            ['skill_level' => 1],
+            ['skill_level' => 2],
+            ['skill_level' => 3],
+            ['skill_level' => 4],
+            ['skill_level' => 5],
+        ))
+        ->create();
+    }
+}
