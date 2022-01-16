d1e8381ab1acf032a7fa415f18665f0ebd979793
ibrahimcadirci <ibrahimh.cadirci@gmail.com>
2022-01-16 14:12:24 +0300

    Creates database table for works

---
+++ database/migrations/2022_01_16_110718_create_works_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',100);
+            $table->integer('time')->default(0);
+            $table->tinyInteger('difficulty')->default(1);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
