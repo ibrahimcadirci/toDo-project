f40dd9e0cfeb07df80c182bf880cfc9051cb466f
ibrahimcadirci <ibrahimh.cadirci@gmail.com>
2022-01-17 09:30:05 +0300

    Edits the Algorithm

--- app/Helper/WorkProgram.php
+++ app/Helper/WorkProgram.php
@@ -15,17 +15,38 @@ class WorkProgram{
                 $assignment             = null;
                 $totalTime              = null;
                 foreach($jobDevelopers as $dev){
-                    if(!is_null($totalTime)){
-                        if($plan->where('dev_id', $dev->id)->sum('time') < $totalTime){
-                            $totalTime              = $plan->where('dev_id', $dev->id)->sum('time');
-                            $assignment             = $dev;
+                    if($this->averageTime > $plan->where('dev_id', $dev->id)->sum('time')){
+                        if(!is_null($totalTime) ){
+                            if($plan->where('dev_id', $dev->id)->sum('time') < $totalTime){
+                                $totalTime              = $plan->where('dev_id', $dev->id)->sum('time');
+                                $assignment             = $dev;
+                            }
+                        }else{
+                            $totalTime                      = $plan->where('dev_id', $dev->id)->sum('time');
+                            $assignment                    = $dev;
                         }
-                    }else{
-                        $totalTime                      = $plan->where('dev_id', $dev->id)->sum('time');
-                        $assignment                    = $dev;
+                    }
+                }
+                if(!isset($assignment->id)){
+                    $jobDevelopers          = $developers->where('skill_level','<',$job['difficulty'])->sortByDESC('skill_level');
+                    foreach($jobDevelopers as $dev){
+                        if($this->averageTime > $plan->where('dev_id', $dev->id)->sum('time')){
+                            if(!is_null($totalTime) ){
+                                if($plan->where('dev_id', $dev->id)->sum('time') < $totalTime){
+                                    $totalTime              = $plan->where('dev_id', $dev->id)->sum('time');
+                                    $assignment             = $dev;
+                                }
+                            }else{
+                                $totalTime                      = $plan->where('dev_id', $dev->id)->sum('time');
+                                $assignment                    = $dev;
+                            }
+                        }
                     }
                 }
                 $week           = $plan->where('dev_id', $assignment->id)->sum('time') > 1 ? (ceil($plan->where('dev_id', $assignment->id)->sum('time') / 45)) : 1; 
+                if( $plan->where('dev_id', $dev->id)->sum('time')){
+
+                }
                 $plan->push(["title" => $job['title'] ,"dev_id" => $assignment->id,"dev" => $assignment, "time"    => round(($job['difficulty'] * $job['time'] / $assignment->skill_level),1), "week" => $week ]);
 
             }
